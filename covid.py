134c194323ade667aa57ab370eb352716685983c
Frank DeSantis <f_desantis@yahoo.com>
2020-03-12 16:43:26 -0700

    Added CSV Reader

--- covid.py
+++ covid.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
-
+import csv 
 
 def main():
-
+   with open("3-10-2020.csv", newline='') as csvfile:
+        reader = csv.DictReader(csvfile, delimiter=',', quotechar='|')
+        
+                
 
 if __name__ == "__main__":
     main()
