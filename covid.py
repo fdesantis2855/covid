ca3c7b3266f734d5d287a8eb743173513a8958db
Frank DeSantis <f_desantis@yahoo.com>
2020-03-13 22:37:53 -0700

    New Function structure

--- covid.py
+++ covid.py
@@ -2,7 +2,7 @@
 import csv
 
 
-def daily_report(day, keyfield):
+def daily_report(day,keyfield):
   
     path = "COVID-19\\csse_covid_19_data\\csse_covid_19_daily_reports\\"
     file_name = (path+day+".csv")
@@ -10,16 +10,18 @@ def daily_report(day, keyfield):
     table = {}
     with open(file_name, "rt", newline='') as csvfile:
         reader = csv.DictReader(csvfile, delimiter=',', quotechar='|')
-
+        
+    #with open("COVID-19\\csse_covid_19_data\\csse_covid_19_daily_reports\\03-13-2020.csv", newline='') as csvfile:
+        #reader = csv.DictReader(csvfile, delimiter=',', quotechar='|')    
+        
         for row in reader:
             table[row[keyfield]] = row
-            print ( row )
+            print( table )
         return table
-
 
         
 if __name__ == "__main__":
-    daily_report("3-12-2020","Country/Region")    
+    daily_report("03-13-2020","Country/Region")    
                
 
 
